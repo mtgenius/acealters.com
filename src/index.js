9ca3a54e57bcd58d8708fbdedc67fe6d787eee6a
Charles Stover <github@charlesstover.com>
2019-03-06 19:48:37 -0800

    unregister service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './register-service-worker';
+import { unregister } from './register-service-worker';
 import App from './components/app/app';
 
 ReactDOM.render(
@@ -12,4 +12,4 @@ if (module.hot) {
   module.hot.accept();
 }
 
-registerServiceWorker();
+unregister();
